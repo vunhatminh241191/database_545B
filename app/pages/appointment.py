1417dd6857896b99323af0b38d3a2f9b24961a72
Minh Vu <vnm241191@gmail.com>
2015-12-08 17:10:28 -0800

    Adding Delete and Update Appointment

--- app/pages/appointment.py
+++ app/pages/appointment.py
@@ -48,8 +48,53 @@ def create_appointment():
 			return redirect(url_for(".creating_new_account", messages=messages))
 
 		else:
-			add_appointment_to_database(db.cursor(), patient_ssn, symptoms)
-			return render_template('making_new_appointment.html', data='Doctor will see you in the moment')
+			appointment_number = add_appointment_to_database(db.cursor(), patient_ssn, symptoms)
+			return render_template('making_new_appointment.html', data=appointment_number)
+
+@app.route("/delete_appointment", methods=['GET', 'POST'])
+def delete_appointment():
+	if request.method == 'GET':
+		return render_template('delete_appointment.html')
+	elif request.method == 'POST':
+		cursor = db.cursor()
+		appointment_number = request.form['appointment_number']
+		cursor.execute("SELECT * from Appointment where appointment_sn=(%s)", (appointment_number, ))
+		data = cursor.fetchone()
+		if data == None:
+			return render_template('delete_appointment.html', data='Appointment did not exist')
+		elif data[1] != None:
+			return render_template('delete_appointment.html', data='You can not delete this appointment')
+		else:
+			cursor.execute("DELETE FROM Appointment Where appointment_sn=(%s)" , (appointment_number, ))
+			db.commit()
+			return render_template('delete_appointment.html', data="We apologizes when we could not help you. Hope you will get well sone!!!")
+
+@app.route("/showing_appointment_update_info/<appointment>", methods=['GET', 'POST'])
+def showing_appointment_update_info(appointment):
+	if request.method == 'GET':
+		cursor = db.cursor()
+
+		# appointment information
+		cursor.execute("SELECT * from Appointment where appointment_sn=(%s)", (appointment, ))
+		appointment_info = cursor.fetchone()
+
+		# medicine of appointment information
+		cursor.execute("SELECT * from Appointment_medicine where appointment_sn=(%s)", (appointment, ))
+		appointment_medicine_info = cursor.fetchone()
+
+		# appointment_doctor info
+		cursor.execute("SELECT * FROM User where ssn=(%s)", (str(appointment_info[1]), ))
+		appointment_doctor_info = cursor.fetchone()
+
+		# appointment_patient_info
+		cursor.execute("SELECT * FROM User where ssn(%s)", (str(appointment_info[2]), ))
+		appointment_patient_info = cursor.fetchone()
+
+		
+		return_data.extend([str(x) for x in data1[1:]])
+		print return_data
+		return render_template('update_appointment.html', data=return_data)
+
 
 def add_appointment_to_database(cursor, patient_ssn, symptoms):
 	# finding min appointment number 
@@ -60,4 +105,4 @@ def add_appointment_to_database(cursor, patient_ssn, symptoms):
 	cursor.execute("""INSERT INTO Appointment VALUES (%s, null, %s, %s, %s, null )"""
 		,(str(current_appointment_number), patient_ssn, symptoms, today.strftime("%m-%d-%Y")))
 	db.commit()
-
+	return current_appointment_number
